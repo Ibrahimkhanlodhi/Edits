9adbafb4f255db059265db6b74395c36e58ef073
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 00:50:34 +0200

    eslint

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,9 +1,18 @@
 module.exports = {
   extends: ["next/core-web-vitals"],
   rules: {
-    // Add your custom rule overrides here
+    // Disable 'import' reserved keyword error
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "ImportDeclaration",
+        message: "The 'import' keyword is reserved, and should not be used in CommonJS files.",
+        enabled: false
+      }
+    ],
+    // You can disable other rules you don't need
     "@next/next/no-img-element": "off",
-    "react/react-in-jsx-scope": "off", // already unnecessary in Next.js
+    "react/react-in-jsx-scope": "off",
     "no-console": "off",
     "no-unused-vars": "off",
   },

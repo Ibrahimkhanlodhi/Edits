2a29f43a3f30905a40f49b1e91a01a0b4bb2f7e9
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 00:52:46 +0200

    eslint

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,18 +1,20 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  parser: "@babel/eslint-parser", // Using Babel parser to handle ESModules
+  extends: ["next/core-web-vitals", "next/typescript"], // Next.js + TypeScript config
+  parserOptions: {
+    ecmaVersion: 2020, // Enable modern ECMAScript features
+    sourceType: "module", // This allows the usage of import/export syntax
+  },
   rules: {
-    // Disable 'import' reserved keyword error
     "no-restricted-syntax": [
       "error",
       {
         selector: "ImportDeclaration",
-        message: "The 'import' keyword is reserved, and should not be used in CommonJS files.",
-        enabled: false
-      }
+        message: "The 'import' keyword is reserved and should not be used in CommonJS files.",
+        enabled: false,
+      },
     ],
-    // You can disable other rules you don't need
-    "@next/next/no-img-element": "off",
-    "react/react-in-jsx-scope": "off",
+    // Add other rules to disable as needed
     "no-console": "off",
     "no-unused-vars": "off",
   },

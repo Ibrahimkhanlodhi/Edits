24bcaaa47346ad5e293a8bf684a376c6921e10af
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 00:42:29 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  extends: [
+    "next/core-web-vitals",
+    "next",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "babel-eslint", // Only if you're using Babel explicitly (not usually needed with Next.js)
+};
 
 export default eslintConfig;

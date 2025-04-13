42fa85d19f18f9e44a9fa67428fe55f3a1e5a184
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 01:41:39 +0200

    f

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     "react/display-name": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-empty-object-type": "off",
+    "no-unused-vars": "off", // Disabling `no-unused-vars`
+    "no-undef": "off", // Disabling `no-undef`
   },
   parserOptions: {
     ecmaVersion: 2020,

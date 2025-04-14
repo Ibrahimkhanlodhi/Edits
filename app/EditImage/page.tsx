aa9d245333529f72289c50680397bcf1162826b5
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 02:09:42 +0200

    f

--- app/EditImage/page.tsx
+++ app/EditImage/page.tsx
@@ -1,35 +1,34 @@
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ClerkClientProvider from "../components/ClerkClientProvider";
 import ImageEditor from "../components/ImageEditor";
 import styles from "../UserButton.module.scss";
 
 export default function Home() {
-  return <>
-  <ClerkProvider>
-    <SignedIn>
-      <div>
-        <ImageEditor/>
-      </div>
-      
-      <div className={styles['user-button-wrapper']}>
-      <UserButton
-        appearance={{
-          elements: {
-            userButtonAvatarBox: {
-              width: '48px',
-              height: '48px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            },
-          },
-        }}
-      />
-    </div>
-    </SignedIn>
-     <SignedOut>
+  return (
+    <ClerkClientProvider>
+      <SignedIn>
+        <div>
+          <ImageEditor/>
+        </div>
+        <div className={styles['user-button-wrapper']}>
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: {
+                  width: '48px',
+                  height: '48px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                },
+              },
+            }}
+          />
+        </div>
+      </SignedIn>
+      <SignedOut>
         <SignInButton />
       </SignedOut>
-  </ClerkProvider>
-  </>
- 
+    </ClerkClientProvider>
+  );
 }

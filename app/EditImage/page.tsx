d22eb2f1241390dfce3c9ea6ca9761f3237a1815
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 02:15:15 +0200

    f

--- app/EditImage/page.tsx
+++ app/EditImage/page.tsx
@@ -1,34 +1,12 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import ClerkClientProvider from "../components/ClerkClientProvider";
+import AuthWrapper from "../components/AuthWrapper";
 import ImageEditor from "../components/ImageEditor";
-import styles from "../UserButton.module.scss";
 
-export default function Home() {
+export default function EditImagePage() {
   return (
-    <ClerkClientProvider>
-      <SignedIn>
-        <div>
-          <ImageEditor/>
-        </div>
-        <div className={styles['user-button-wrapper']}>
-          <UserButton
-            appearance={{
-              elements: {
-                userButtonAvatarBox: {
-                  width: '48px',
-                  height: '48px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                },
-              },
-            }}
-          />
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-    </ClerkClientProvider>
+    <AuthWrapper>
+      <div>
+        <ImageEditor />
+      </div>
+    </AuthWrapper>
   );
 }
---
+++ app/components/AuthWrapper.tsx
@@ -0,0 +1,38 @@
+
+'use client';
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ClerkClientProvider from "./ClerkClientProvider";
+import styles from "../UserButton.module.scss"
+
+interface AuthWrapperProps {
+  children: React.ReactNode;
+}
+
+export default function AuthWrapper({ children }: AuthWrapperProps) {
+  return (
+    <ClerkClientProvider>
+      <SignedIn>
+        {children}
+        <div className={styles['user-button-wrapper']}>
+<UserButton
+appearance={{
+elements: {
+userButtonAvatarBox: {
+width: '48px',
+height: '48px',
+display: 'flex',
+justifyContent: 'center',
+alignItems: 'center',
+ },
+ },
+ }}
+/>
+</div>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </ClerkClientProvider>
+  );
+}

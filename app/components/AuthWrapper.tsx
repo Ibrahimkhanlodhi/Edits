e8cd515d43cc45c6d6cc51c4a0e2a07fb65c9e70
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 03:22:28 +0200

    f

--- app/components/AuthWrapper.tsx
+++ app/components/AuthWrapper.tsx
@@ -1,5 +1,6 @@
 
 'use client';
+import React from "react";
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import ClerkClientProvider from "./ClerkClientProvider";

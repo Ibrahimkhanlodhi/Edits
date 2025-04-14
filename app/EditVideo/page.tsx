378ea93de8ca2ab471e9b42fe99e29344509faab
Ibrahim <ibrahimkhanlodhi4@gmail.com>
2025-04-14 02:18:16 +0200

    f

--- app/EditVideo/page.tsx
+++ app/EditVideo/page.tsx
@@ -2,41 +2,21 @@
 
 
 import dynamic from "next/dynamic";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import styles from "../UserButton.module.scss";
+
+import AuthWrapper from "../components/AuthWrapper";
 
 
 // Dynamically import the component that uses FFmpeg, ensuring it's only run on the client-side.
 const VideoEditor = dynamic(() => import("../components/VideoEditor"), { ssr: false });
 
 export default function Home() {
-  return <>
-  <ClerkProvider>
-    <SignedIn>
+  return (
+    <AuthWrapper>
       <div>
-        <VideoEditor/>
+        <VideoEditor />
       </div>
+    </AuthWrapper>
+  )
       
-      <div className={styles['user-button-wrapper']}>
-      <UserButton
-        appearance={{
-          elements: {
-            userButtonAvatarBox: {
-              width: '48px',
-              height: '48px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            },
-          },
-        }}
-      />
-    </div>
-    </SignedIn>
-     <SignedOut>
-        <SignInButton />
-      </SignedOut>
-  </ClerkProvider>
-  </>
  
 }
